e0871ed5b3b2f9d6a3861a1323285cc01bc917be
Colin Moran <colin.mtech@gmail.com>
2023-04-13 17:18:21 -0400

    feat: add yaml to ensure_installed ts list

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "lua", "java", "scala", "go", "rust", "typescript", "vim", "vimdoc" },
+    ensure_installed = { "lua", "java", "scala", "go", "rust", "typescript", "vim", "vimdoc", "yaml" },
   },
 }
